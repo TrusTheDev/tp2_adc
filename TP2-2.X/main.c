25f05ba461bf6f44740a504ea74b2b686aa42eb0
TrusTheDev <123142834+TrusTheDev@users.noreply.github.com>
2026-05-08 18:47:32 -0300

    reformateo de código, logica de restauración implementada

--- TP2-2.X/main.c
+++ TP2-2.X/main.c
@@ -8,41 +8,49 @@ Completar el código faltante.
 #include <stdio.h>
 #include <stdlib.h>
 #include "kernel.h"
-void procesoA(void)
-{
-    int x,y,z;
+
+void procesoA(void) {
+    int x, y, z;
     x = 2;
     y = 4;
-    while(1) {
-         z = x + y;
-          x = x + 2;
-         y = z;
-     }
+    while (1) {
+        z = x + y;
+        x = x + 2;
+        y = z;
+    }
 }
-void procesoB(void)
-{
-    int a,b,c;
+
+void procesoB(void) {
+    int a, b, c;
     a = 1;
     b = 3;
-    while(1) {
+    while (1) {
         c = a * b;
         a = a + 2;
     }
 }
-void procesoC(void)
-{
-    int r,s,t;
+
+void procesoC(void) {
+    int r, s, t;
     r = 1;
     s = r;
-    while(1) {
+    while (1) {
         t = r + s;
         t >>= 1;
         r = r + 1;
         s = s + 4;
     }
 }
+
+unsigned int estadoA[12];
+unsigned int estadoB[12];
+unsigned int estadoC[12];
+
 int main(int argc, char** argv) {
+    estadoA[0] = procesoA;
+    estadoB[0] = procesoB;
+    estadoC[0] = procesoC;
     boot();
-      procesoA();
+    procesoA();
     return (EXIT_SUCCESS);
 }
